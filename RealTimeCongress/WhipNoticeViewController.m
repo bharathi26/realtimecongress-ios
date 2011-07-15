a2e529005b75a132893d2f9b27ef9cee09cbe018
Tom Tsai <hijoumake@gmail.com>
2011-07-15 14:39:33 -0700

    Fixed issue with Whip Notices requesting data with incorrect parameters

--- RealTimeCongress/WhipNoticeViewController.m
+++ RealTimeCongress/WhipNoticeViewController.m
@@ -260,7 +260,6 @@
     NSDictionary *requestParameters = [[NSDictionary alloc] initWithObjectsAndKeys:
                                        [NSString stringWithFormat:@"%@", REQUEST_PAGE_SIZE], @"per_page",
                                        @"for_date", @"sort",
-                                       @"desc", @"order",
                                        nil];
     SunlightLabsRequest *dataRequest = [[SunlightLabsRequest alloc] initRequestWithParameterDictionary:requestParameters APICollection:Documents APIMethod:nil];
     
