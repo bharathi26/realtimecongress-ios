6a913134cd9f936388a93ea9f042975fd0f39d61
Tom Tsai <hijoumake@gmail.com>
2011-08-08 17:15:49 -0700

    Added title to Documents List view controller.

--- DocumentsListViewController.m
+++ DocumentsListViewController.m
@@ -32,7 +32,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    self.title = @"Documents";
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
