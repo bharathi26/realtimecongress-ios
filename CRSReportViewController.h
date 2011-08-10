3cd241c8ec07a415efe9ad0e54c4f7895745b13f
Tom Tsai <hijoumake@gmail.com>
2011-08-10 18:29:04 -0500

    Added skeleton CRS Report view controller

---
+++ CRSReportViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRSReportViewController.h
+//  RealTimeCongress
+//
+//  Created by Tom Tsai on 8/10/11.
+//  Copyright 2011 Sunlight Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRSReportViewController : UITableViewController
+
+@end
