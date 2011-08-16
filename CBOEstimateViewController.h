12d9d7f06377bb14925e5ebce41d92a32e358b44
Tom Tsai <hijoumake@gmail.com>
2011-08-16 15:41:38 -0400

    Added files for CBO Estimate view controller

---
+++ CBOEstimateViewController.h
@@ -0,0 +1,13 @@
+//
+//  CBOEstimateViewController.h
+//  RealTimeCongress
+//
+//  Created by Tom Tsai on 8/16/11.
+//  Copyright 2011 Sunlight Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBOEstimateViewController : UITableViewController
+
+@end
