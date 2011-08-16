8d8661cf6a2c49833d1b09ebb0ec5e96acfa0946
Tom Tsai <hijoumake@gmail.com>
2011-08-16 15:46:43 -0400

    Copied UI Action methods from CRS Reports into CBO Estimates

--- CBOEstimateViewController.h
+++ CBOEstimateViewController.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "JSONKit.h"
+#import "SunlightLabsConnection.h"
+#import "Reachability.h"
 
-@interface CBOEstimateViewController : UITableViewController
+#define REQUEST_PAGE_SIZE @"100"
+#define CELL_WIDTH 260
+
+@interface CBOEstimateViewController : UITableViewController {
+@private
+    UIActivityIndicatorView *loadingIndicator;
+    NSMutableDictionary *reportDaysDictionary;
+    NSMutableArray *sectionDataArray;
+    NSMutableArray *reportDaysArray;
+    SunlightLabsConnection *connection;
+    Reachability *reachabilityInfo;
+}
+
+- (void) refresh;
+- (void) parseData: (NSNotification *)notification;
+- (void) parseCachedData: (NSData *) data;
+- (void) retrieveData;
 
 @end
--- CBOEstimateViewController.m
+++ CBOEstimateViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "CBOEstimateViewController.h"
+#import "WhipNoticesWebViewController.h"
+#import "SunlightLabsRequest.h"
+#import "JSONKit.h"
+#import "GANTracker.h"
+#import "Reachability.h"
 
 @implementation CBOEstimateViewController
 
@@ -154,6 +159,251 @@
      [self.navigationController pushViewController:detailViewController animated:YES];
      [detailViewController release];
      */
+}
+
+#pragma mark - UI Actions
+
+- (void) refresh
+{
+    // Check network reachability. If unreachable, display alert view. Otherwise, retrieve data
+    NetworkStatus internetStatus = [reachabilityInfo currentReachabilityStatus];
+    if (internetStatus != NotReachable) {
+        //Disable scrolling while data is loading
+        self.tableView.scrollEnabled = NO;
+        
+        //Animate the activity indicator and network activity indicator when loading data
+        [loadingIndicator startAnimating];
+        
+        NSError *error;
+        //Register a page view to the Google Analytics tracker
+        if (![[GANTracker sharedTracker] trackPageview:@"/crs_reports"
+                                             withError:&error]) {
+            // Handle error here
+        }
+        
+        // Generate request URL using Sunlight Labs Request class
+        NSDictionary *requestParameters = [[NSDictionary alloc] initWithObjectsAndKeys:
+                                           [NSString stringWithFormat:@"%@", REQUEST_PAGE_SIZE], @"per_page",
+                                           @"crs_report", @"document_type",
+                                           nil];
+        SunlightLabsRequest *dataRequest = [[SunlightLabsRequest alloc] initRequestWithParameterDictionary:requestParameters APICollection:Documents APIMethod:nil];
+        connection = [[SunlightLabsConnection alloc] initWithSunlightLabsRequest:dataRequest];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(parseData:) name:SunglightLabsRequestFinishedNotification object:connection];
+        [connection sendRequest];
+        NSLog(@"User initiated refresh. Use network.");
+    }
+    else {
+        NSLog(@"The internet is inaccessible.");
+        
+        UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"The internet is currently inaccessible."
+                                                         message:@"Please check your connection and try again."
+                                                        delegate:self
+                                               cancelButtonTitle:@"Ok"  
+                                               otherButtonTitles:nil];
+        
+        [alert show];
+        [alert release];
+    }
+}
+
+- (void) parseData: (NSNotification *)notification
+{
+    //Release connection upon data receiption
+    [connection release];
+    connection = nil;
+    
+    // Clear array when new data received
+    [reportDaysArray removeAllObjects];
+    
+    //Assign received data
+    NSDictionary *items = [notification userInfo];
+    
+    static NSDateFormatter * dateFormatter;
+    static NSDateFormatter *updateDayFormatter;
+    
+    if (dateFormatter == nil) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+    }
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+    [dateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
+    
+    if (updateDayFormatter == nil) {
+        updateDayFormatter = [[NSDateFormatter alloc] init];
+    }
+    [updateDayFormatter setDateFormat:@"EEEE, MMMM dd"];
+    
+    for (id update in [items objectForKey:@"documents"]) {
+        NSDate * date = [dateFormatter dateFromString:[update objectForKey:@"released_at"]];
+        
+        // Check if the date has been added to update days array. Add it if it hasn't.
+        NSString *updateDay = [updateDayFormatter stringFromDate: date];
+        if (![reportDaysArray containsObject: updateDay]) {
+            [reportDaysArray addObject:updateDay];
+        }
+        
+        // Check if there is an array for the update day. If there isn't, create an array to store updates for that day. 
+        if ([reportDaysDictionary objectForKey:updateDay] == nil) {
+            [reportDaysDictionary setObject:[NSMutableArray array] forKey:updateDay];
+            // Add the update to its respective array
+            [[reportDaysDictionary objectForKey:updateDay] addObject:update];
+        }
+        else {
+            // Add the update to its respective array if it already exists
+            [[reportDaysDictionary objectForKey:updateDay] addObject:update];
+        }
+    }
+    
+    sectionDataArray = [[NSMutableArray alloc] initWithCapacity:20];
+    
+    for (NSString *updateDayString in reportDaysArray) {
+        if (updateDayString != @"") {
+            [sectionDataArray addObject:[reportDaysDictionary objectForKey:updateDayString]];
+        }
+    }
+    
+    //Reload the table once data retrieval is complete
+    [self.tableView reloadData];
+    
+    //Hide the activity indicator and network activity indicator once loading is complete
+    [loadingIndicator stopAnimating];
+    
+    //Re-enable scrolling once loading is complete and the loading indicator disappears
+    self.tableView.scrollEnabled = YES;
+}
+
+- (void) parseCachedData:(NSData *)data {
+    NSDictionary *items = [[JSONDecoder decoder] objectWithData:data];
+    
+    static NSDateFormatter * dateFormatter;
+    static NSDateFormatter *updateDayFormatter;
+    
+    if (dateFormatter == nil) {
+        dateFormatter = [[NSDateFormatter alloc] init];
+    }
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
+    [dateFormatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
+    
+    if (updateDayFormatter == nil) {
+        updateDayFormatter = [[NSDateFormatter alloc] init];
+    }
+    [updateDayFormatter setDateFormat:@"EEEE, MMMM dd"];
+    
+    for (id update in [items objectForKey:@"documents"]) {
+        NSDate * date = [dateFormatter dateFromString:[update objectForKey:@"released_at"]];
+        
+        // Check if the date has been added to update days array. Add it if it hasn't.
+        NSString *updateDay = [updateDayFormatter stringFromDate: date];
+        if (![reportDaysArray containsObject: updateDay]) {
+            [reportDaysArray addObject:updateDay];
+        }
+        
+        // Check if there is an array for the update day. If there isn't, create an array to store updates for that day. 
+        if ([reportDaysDictionary objectForKey:updateDay] == nil) {
+            [reportDaysDictionary setObject:[NSMutableArray array] forKey:updateDay];
+            // Add the update to its respective array
+            [[reportDaysDictionary objectForKey:updateDay] addObject:update];
+        }
+        else {
+            // Add the update to its respective array if it already exists
+            [[reportDaysDictionary objectForKey:updateDay] addObject:update];
+        }
+    }
+    
+    sectionDataArray = [[NSMutableArray alloc] initWithCapacity:20];
+    
+    for (NSString *updateDayString in reportDaysArray) {
+        [sectionDataArray addObject:[reportDaysDictionary objectForKey:updateDayString]];
+    }
+    
+    //Reload the table once data retrieval is complete
+    [self.tableView reloadData];
+    
+    //Hide the activity indicator and network activity indicator once loading is complete
+    [loadingIndicator stopAnimating];
+    
+    //Re-enable scrolling once loading is complete and the loading indicator disappears
+    self.tableView.scrollEnabled = YES;
+}
+
+- (void) retrieveData
+{
+    NSError *error;
+    //Register a page view to the Google Analytics tracker
+    if (![[GANTracker sharedTracker] trackPageview:@"/crs_reports"
+                                         withError:&error]) {
+        // Handle error here
+    }
+    
+    // Generate request URL using Sunlight Labs Request class
+    NSDictionary *requestParameters = [[NSDictionary alloc] initWithObjectsAndKeys:
+                                       [NSString stringWithFormat:@"%@", REQUEST_PAGE_SIZE], @"per_page",
+                                       @"crs_report", @"document_type",
+                                       nil];
+    SunlightLabsRequest *dataRequest = [[SunlightLabsRequest alloc] initRequestWithParameterDictionary:requestParameters APICollection:Documents APIMethod:nil];
+    
+    NSCachedURLResponse *cachedResponse = [[NSURLCache sharedURLCache] cachedResponseForRequest:[dataRequest request]];
+    NSDate *responseAge = [[cachedResponse userInfo] objectForKey:@"CreationDate"];
+    NSDate *currentDate = [NSDate date];
+    
+    //Animate the activity indicator and network activity indicator when loading data
+    [loadingIndicator startAnimating];
+    
+    // Check if there is an unexpired cached response
+    if ((cachedResponse != nil) && ([currentDate timeIntervalSinceDate:responseAge] < 300)) {
+        [self parseCachedData:[[[NSURLCache sharedURLCache] cachedResponseForRequest:[dataRequest request]] data]];
+        NSLog(@"Cached data loaded");
+    }
+    else{
+        // Check network reachability. If unreachable, display alert view. Otherwise, retrieve data
+        NetworkStatus internetStatus = [reachabilityInfo currentReachabilityStatus];
+        if (internetStatus != NotReachable) {
+            connection = [[SunlightLabsConnection alloc] initWithSunlightLabsRequest:dataRequest];
+            [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(parseData:) name:SunglightLabsRequestFinishedNotification object:connection];
+            [connection sendRequest];
+            NSLog(@"No cached data. Use network.");
+        }
+        else {
+            NSLog(@"The internet is inaccessible.");
+            
+            UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"The internet is currently inaccessible."
+                                                             message:@"Please check your connection and try again."
+                                                            delegate:self
+                                                   cancelButtonTitle:@"Ok"  
+                                                   otherButtonTitles:nil];
+            
+            [alert show];
+            [alert release];
+        }
+    }
+}
+
+- (void) reachabilityChanged {
+    NetworkStatus internetStatus = [reachabilityInfo currentReachabilityStatus];
+    if (internetStatus == NotReachable) {
+        NSLog(@"The internet is inaccessible.");
+        
+        UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"The internet is currently inaccessible."
+                                                         message:@"Please check your connection and try again."
+                                                        delegate:self
+                                               cancelButtonTitle:@"Ok"  
+                                               otherButtonTitles:nil];
+        
+        [alert show];
+        [alert release];
+        
+    }
+    else {
+        NSLog(@"The internet is now accessible.");
+        
+        UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Internet now accessible."
+                                                         message:@"Internet now accessible."
+                                                        delegate:self
+                                               cancelButtonTitle:@"Ok"  
+                                               otherButtonTitles:nil];
+        
+        [alert show];
+        [alert release];
+    }
 }
 
 @end
