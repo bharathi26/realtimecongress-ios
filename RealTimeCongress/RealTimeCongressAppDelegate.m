528003c58481a94c191fd248daf894cc5483c965
Tom Tsai <hijoumake@gmail.com>
2011-08-17 12:27:50 -0400

    Added an additional call to Google Analytics tracker object in applicationWillEnterForeground to improve dispatch performance

--- RealTimeCongress/RealTimeCongressAppDelegate.m
+++ RealTimeCongress/RealTimeCongressAppDelegate.m
@@ -65,6 +65,7 @@ static const NSInteger kGANDispatchPeriodSec = -1; //Manual dispatch
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
+    [[GANTracker sharedTracker] dispatch];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
