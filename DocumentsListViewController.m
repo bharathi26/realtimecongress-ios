dc1bb0b3e7fbbf8e9999e8e7fb4333480807cfac
Tom Tsai <hijoumake@gmail.com>
2011-08-17 10:00:03 -0400

    Removed extraneous comments from Documents List

--- DocumentsListViewController.m
+++ DocumentsListViewController.m
@@ -26,8 +26,6 @@
 {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
 }
 
 #pragma mark - View lifecycle
@@ -36,18 +34,11 @@
 {
     [super viewDidLoad];
     self.title = @"Documents";
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
