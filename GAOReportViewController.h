c240a4981f8a93a5d522e4568540abdefc06c41f
Tom Tsai <hijoumake@gmail.com>
2011-08-17 09:34:44 -0400

    Added files for GAO Report view controller

---
+++ GAOReportViewController.h
@@ -0,0 +1,13 @@
+//
+//  GAOReportViewController.h
+//  RealTimeCongress
+//
+//  Created by Tom Tsai on 8/17/11.
+//  Copyright 2011 Sunlight Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GAOReportViewController : UITableViewController
+
+@end
